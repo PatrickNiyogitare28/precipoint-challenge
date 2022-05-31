69f96d2780b40e76ce54f9b2e8b47ea96a09d8ba
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2022-05-31 02:21:28 +0200

    chore(courses): added list courses

--- src/app/views/courses/courses.module.ts
+++ src/app/views/courses/courses.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { StudentsComponent } from './students/students.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseComponent } from './course/course.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    StudentsComponent
+    StudentsComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     CommonModule,
